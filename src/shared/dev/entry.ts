110da8b897133e98ecaaa9cca0ec424933b6027d
IceOfSummer <xu2237803016@outlook.com>
2025-01-23 17:02:16 +0800

    chore: update dependency

--- src/shared/dev/entry.ts
+++ src/shared/dev/entry.ts
@@ -1,17 +1,7 @@
-import { createBridgePeerClient } from 'cross-iframe-rpc'
+import { setupInMainWindow } from 'cross-iframe-rpc'
 
 const iframe = document.getElementById('iframe') as HTMLIFrameElement
-createBridgePeerClient({
-  target: chrome,
-  poster: {
-    postMessage(str) {
-      iframe.contentWindow!.postMessage(str, '*')
-    },
-    addEventListener(name, callback) {
-      addEventListener(name, callback)
-    },
-    removeEventListener(name, callback) {
-      removeEventListener(name, callback)
-    }
-  }
+setupInMainWindow({
+  iframe,
+  delegateTarget: chrome,
 })
--- src/shared/dev/iframe-setup.ts
+++ src/shared/dev/iframe-setup.ts
@@ -1,23 +1,8 @@
-import {createBridePeerClientWithTypeOnly} from "cross-iframe-rpc";
+import { setupInIframe } from "cross-iframe-rpc";
 
 export default function iframeSetup() {
   if (process.env.NODE_ENV === 'development') {
-    window.chrome = createBridePeerClientWithTypeOnly<typeof chrome>({
-      poster: {
-        postMessage(str) {
-          window.parent.window.postMessage(str, '*')
-        },
-        addEventListener(name, callback) {
-          window.addEventListener(name, (evt) => {
-            callback(evt)
-          })
-        },
-        removeEventListener(name, callback) {
-          window.removeEventListener(name, callback)
-        }
-      }
-    })
+    window.chrome = setupInIframe<typeof chrome>()
   }
-
 }
 
